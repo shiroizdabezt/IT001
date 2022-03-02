caa3b2ec5c83831745a5980fc72099e78dd7b080
shiroizdabezt <codykhoanguyen@gmail.com>
2022-03-02 16:10:53 +0700

    Create 07.cpp

---
+++ Struct/07.cpp
@@ -0,0 +1,66 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+###End banned keyword*/
+
+#include <iomanip>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+#define MAXN 100
+
+struct SinhVien {
+    char MASV[10];
+    char HoTen[100];
+    char NgaySinh[12];
+    char GioiTinh;
+    float DiemToan, DiemLy, DiemHoa, DTB;
+};
+
+//###INSERT CODE HERE -
+void Nhap ( SinhVien a[] , int &n )
+{
+    cin >> n ;
+    cin.ignore () ;
+    for ( int i = 0 ; i < n ; i++ )
+        {
+            cin.getline ( a [ i ].MASV , 10 ) ;
+            cin.getline ( a [ i ].HoTen , 100 ) ;
+            cin.getline ( a [ i ].NgaySinh , 12 ) ;
+            cin >> a [ i ].GioiTinh ;
+            cin >> a [ i ].DiemToan >> a [ i ].DiemLy >> a [ i ].DiemHoa ;
+            a [ i ].DTB = ( a [ i ].DiemToan + a [ i ].DiemLy + a [ i ].DiemHoa ) / 3 ;
+            cin.ignore () ;
+        }
+}
+
+void SapXepGiamTheoMASV ( SinhVien a[] , int n )
+{
+    for ( int i = 0 ; i < n ; i++ )
+        {
+            /* if ( strcmp ( maxmssv , a[ i ] .MASV ) < 0 ) 
+                strcpy ( maxmssv , a [ i ] .MASV )  */
+            if ( strcmp ( a [ i ].MASV , a [ i - 1 ].MASV ) > 0 ) 
+                {
+                    swap ( a [ i ] , a [ i - 1 ] ) ;
+                    i = 0 ;
+                }
+        }
+}
+void Xuat ( SinhVien a[] , int n )
+{
+    for ( int i = 0 ; i < n ; i++ )
+        {
+            cout << a [ i ].MASV << '\t' << a [ i ].HoTen << '\t' << a [ i ].NgaySinh << '\t' <<  a [ i ].GioiTinh << '\t' << a [ i ].DiemToan << '\t' << a [ i ].DiemLy << '\t' << a [ i ].DiemHoa;
+            cout << '\t' << setprecision ( 3 ) << a [ i ].DTB << '\n' ;
+        }
+}
+
+
+int main() {
+    SinhVien A[MAXN];
+    int n;
+    Nhap(A, n);
+    SapXepGiamTheoMASV(A, n);
+    Xuat(A, n);
+    return 0;
+}
